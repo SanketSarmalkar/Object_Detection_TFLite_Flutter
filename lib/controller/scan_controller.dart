667b3c707fd83dfd4ceaf3005952391047cc310a
SanketSarmalkar <sanket.sarmalkar2002@gmail.com>
2023-08-26 21:16:22 +0530

    feat/tflite-connection

--- lib/controller/scan_controller.dart
+++ lib/controller/scan_controller.dart
@@ -1,4 +1,5 @@
 import 'package:camera/camera.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -6,13 +7,16 @@ import 'package:permission_handler/permission_handler.dart';
 class ScanController extends GetxController {
   late CameraController cameraController;
   late List<CameraDescription> cameras;
+  late CameraImage cameraImage;
 
   var isCameraReady = false.obs;
+  var cameraCount = 0;
 
   @override
   void onInit() {
     super.onInit();
     initCamera();
+    initTflite();
   }
 
   @override
@@ -25,7 +29,15 @@ class ScanController extends GetxController {
     if (await Permission.camera.request().isGranted) {
       cameras = await availableCameras();
       cameraController = CameraController(cameras[0], ResolutionPreset.max);
-      await cameraController.initialize();
+      await cameraController.initialize().then((value) {
+        cameraController.startImageStream((image) {
+          cameraCount++;
+          if (cameraCount % 100 == 0) {
+            cameraCount = 0;
+            objectDetecter(image);
+          }
+        });
+      });
       isCameraReady.value = true;
       update();
     } else {
@@ -38,6 +50,37 @@ class ScanController extends GetxController {
         colorText: Colors.white,
         icon: const Icon(Icons.camera_alt),
       );
+    }
+  }
+
+  initTflite() async {
+    var res = await Tflite.loadModel(
+      model: 'assets/model.tflite',
+      labels: 'assets/labels.txt',
+      isAsset: true,
+      numThreads: 1,
+      useGpuDelegate: false,
+    );
+    debugPrint('Result: $res');
+  }
+
+  objectDetecter(CameraImage image) async {
+    debugPrint("First Holla");
+    var detector = await Tflite.runModelOnFrame(
+        bytesList: image.planes.map((plane) {
+          return plane.bytes;
+        }).toList(),
+        imageHeight: image.height,
+        imageWidth: image.width,
+        imageMean: 127.5,
+        imageStd: 127.5,
+        rotation: 90,
+        numResults: 1,
+        threshold: 0.4,
+        asynch: true);
+    debugPrint("HOlla");
+    if (detector != null) {
+      debugPrint('Detector: $detector');
     }
   }
 }
